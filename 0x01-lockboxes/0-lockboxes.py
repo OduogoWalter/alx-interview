df235370d272ea1b459b8c45380a8aa54407ed15
OduogoWalter <oduogowalter@gmail.com>
2024-10-08 18:11:07 +0300

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def canUnlockAll(boxes):
+    opened_boxes = set([0])
+    keys = [0]
+    
+    while keys:
+        current_box = keys.pop()
+        for key in boxes[current_box]:
+            if key not in opened_boxes and key < len(boxes):
+                opened_boxes.add(key)
+                keys.append(key)
+                
+    return len(opened_boxes) == len(boxes)
